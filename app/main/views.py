51f3e843cc339362d0e3c19ac87d235ee47a5d69
jbc <jhovany.badc@gmail.com>
2020-05-21 20:18:35 -0500

    Se agregó funcionalidad para listar todos los productos de un usuario en /mis-productos

--- app/main/views.py
+++ app/main/views.py
@@ -3,13 +3,11 @@ from flask_login import login_required, current_user
 from . import main
 from .forms import LoginForm, RegistroUsuarioForm
 from .. import db
-from ..models import Usuario, Cliente
-from hashlib import md5
+from ..models import Usuario, Cliente, Producto
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     if current_user.is_authenticated:
-        print(current_user)
         return redirect(url_for('main.menu'))
     else:
         return render_template('index.html')
@@ -18,3 +16,12 @@ def index():
 @login_required
 def menu():
     return render_template('menu.html')
+
+@main.route('/mis-productos')
+@login_required
+def mis_productos():
+    mis_productos = []
+    for cliente in current_user.clientes.all():
+        for producto in cliente.productos.all():
+            mis_productos.append(producto)
+    return render_template('mis_productos.html', mis_productos=mis_productos)
